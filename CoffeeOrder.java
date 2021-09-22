8094ea80bbf829461be3fd96f7ecf6c493551899
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-09-21 17:52:10 -0700

    Update CoffeeOrder.java

--- CoffeeOrder.java
+++ CoffeeOrder.java
@@ -6,5 +6,21 @@
  */
 
 public class CoffeeOrder {
-
+  
+  /**
+   * You must have this main() to run test cases, so code your functions and call it here
+   * For more hints about Input/Output, view : https://canvas.uw.edu/courses/1492302/assignments/6674078
+   */
+  public static void main(String[] args) {
+    //TODO
+  }
+  
+  /**
+   * Return the price of a   espresso drink based on their coffee type and size
+   * 
+   */
+  public static double getPrice(int coffeeType, int size) {
+    //TODO
+    return 0.0;
+  }
 }
